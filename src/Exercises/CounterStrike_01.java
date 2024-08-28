90c6a8e58ac91542ca3ba55d4e3c65fb72825f06
beloslavageorgieva <beloslava.mg@gmail.com>
2024-08-28 12:53:08 +0300

    change

--- src/Exercises/CounterStrike_01.java
+++ src/Exercises/CounterStrike_01.java
@@ -9,22 +9,24 @@ public class CounterStrike_01 {
         int energy = Integer.parseInt(scanner.nextLine());
         int wonBattles = 0;
 
+        // Loop until "End of battle" or not enough energy
         while (true) {
-            String input = scanner.next();
+            String input = scanner.nextLine();
+
             if ("End of battle".equals(input)) {
-                System.out.printf("Won battles: %d. Energy left: %d", wonBattles, energy);
+                System.out.printf("Won battles: %d. Energy left: %d%n", wonBattles, energy);
                 break;
             }
 
             int distance = Integer.parseInt(input);
             if (energy < distance) {
-                System.out.printf("Not enough energy! Game ends with %d won battles and %d energy", wonBattles, energy);
+                System.out.printf("Not enough energy! Game ends with %d won battles and %d energy%n", wonBattles, energy);
                 break;
             }
-
             energy -= distance;
             wonBattles++;
 
+            // Every third won battle increases the energy by the current count of won battles
             if (wonBattles % 3 == 0) {
                 energy += wonBattles;
             }
